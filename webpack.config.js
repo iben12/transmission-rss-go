4212a4f8714760ad5ad8637d88b23397356a9487
Bence Ivady <iben@iben.hu>
2021-12-14 10:24:26 +0100

    add frontend and static content

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const sass = new ExtractTextPlugin("style.css");
+
+module.exports = {
+  entry: ["./frontend/js/app.js"],
+  output: {
+    path: __dirname + "/static/assets",
+    filename: "app.js"
+  },
+  optimization: {
+    usedExports: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  corejs: "2",
+                  useBuiltIns: "entry"
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: sass.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                minimize: true
+              }
+            },
+            "sass-loader"
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [sass],
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.common.js"
+    }
+  },
+  devtool: "source-map",
+  watch: false
+};
