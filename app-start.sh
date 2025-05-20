a3af47e9c9d4f6bf8490366da35f3d77c27b4187
Bence Ivady <bence.ivady@emarsys.com>
2025-05-20 13:24:52 +0200

    Optional proxy

--- app-start.sh
+++ app-start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-/app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=cloudrun-app
-echo Tailscale started
-ALL_PROXY=socks5://localhost:1055/ /app/trss
+if [ "$USE_PROXY" == "true" ]; then
+    /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+    /app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=cloudrun-app
+    echo Tailscale started
+    HTTP_PROXY=socks5://localhost:1055/ HTTPS_PROXY=socks5://localhost:1055/ /app/trss
+else
+    /app/trss
+fi
